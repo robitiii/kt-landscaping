135421e6d01c1018ac42b1f18c0b34e215350760
Robert Erady <137341984+robitiii@users.noreply.github.com>
2025-06-20 15:04:46 +0200

    removed that icon with bouncing animation.

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -28,11 +28,11 @@ const HeroSection = () => {
         </div>
       </div>
       
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
+      {/* <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
         <div className="w-6 h-10 border-2 border-nature-500 rounded-full flex justify-center">
           <div className="w-1 h-3 bg-nature-500 rounded-full mt-2"></div>
         </div>
-      </div>
+      </div> */}
     </section>
   );
 };
